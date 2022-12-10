d5256f6b84dd819a5fc8a903dac23b9cba6971af
irem <cemreirem@hotmail.com>
2022-12-10 18:52:38 +0300

    Initial commit

---
+++ notebooks/test.py
@@ -0,0 +1,8 @@
+sehir=["ankara","istanbul","izmir","bursa","antalya"]
+yaş=[25,45,21,52,67,34,53]
+sonuc=[]
+
+sonuc.append("Mehmet")
+sonuc.append(yaş[2])
+
+print(f"{sonuc[0]} {sonuc[1]} yaşında bir yazılımcı olmaktadır.")
